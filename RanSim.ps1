9b70dce7aa309b7ca4ae3856e73909d818b4832e
PurpleWaveIO <104524404+PurpleWaveIO@users.noreply.github.com>
2023-06-12 13:40:30 -0400

    Update RanSim.ps1

--- RanSim.ps1
+++ RanSim.ps1
@@ -28,13 +28,13 @@ your simulation, but you also have the option to decrypt the files with the same
 
 # Define parameters and their defaults
 param([string]$Mode,
-      [string]$TargetPath = "C:\RanSim",
-      [string]$Extension = ".encrypted",
+      [string]$TargetPath = "C:\bbsim",
+      [string]$Extension = ".blackbasta",
       [string]$Key = "Q5KyUru6wn82hlY9k8xUjJOPIC9da41jgRkpt21jo2L="
 )
 
 # Define target file types
-$TargetFiles = '*.pdf','*.xls*','*.ppt*','*.doc*','*.accd*','*.rtf','*.txt','*.csv','*.jpg','*.jpeg','*.png','*.gif','*.avi','*.midi','*.mov','*.mp3','*.mp4','*.mpeg','*.mpeg2','*.mpeg3','*.mpg','*.ogg'
+$TargetFiles = '*.exe*','*.dll*','*.iso*','*.doc*','*.accd*','*.rtf','*.txt','*.csv','*.jpg','*.jpeg','*.png','*.gif','*.avi','*.midi','*.mov','*.mp3','*.mp4','*.mpeg','*.mpeg2','*.mpeg3','*.mpg','*.ogg'
 
 # Import FileCryptography module
 Import-Module "$PSScriptRoot\FileCryptography.psm1"
